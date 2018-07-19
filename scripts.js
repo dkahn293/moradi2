93a74023c38d4b7e6c096a1797fddaf7c1d55245
david kahn <davidkahn321@gmail.com>
2018-07-19 13:21:45 -0700

    messenger 2

--- scripts.js
+++ scripts.js
@@ -31,19 +31,4 @@ function createSticky(sticky) {
 
 //facebook messenger
 
-window.fbAsyncInit = function () {
-    FB.init({
-        appId: '252416415554866',
-        autoLogAppEvents: true,
-        xfbml: true,
-        version: 'v3.0'
-    });
-};
 
-(function (d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) { return; }
-    js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
